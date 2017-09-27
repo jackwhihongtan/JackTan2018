65224c3b5a0db64755089089fb508564f8adeb26
jackwhihongtan <18tanj98@millshighsch.org>
2017-09-27 10:24:07 -0700

    Comments and name changes made

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -1,156 +1,157 @@
+//Jack Tan, 9/27/17. 2nd Period
 
 public class Calculate {
 	//square the input
 		public static int square (int operand) {
-			int square = operand * operand;
+			int square = operand * operand; // I tried to compute X^2
 			return square;
 		}
 		public static int cube (int alpha) {
-			int cube = alpha * alpha * alpha;
-			return cube;
+			int cube = alpha * alpha * alpha; // = X^3
+			return cube; 
 		}
-		public static double average (double beta1, double beta2) {
+		public static double average (double beta1, double beta2) { //can use in stats, takes only 2 values can change for more
 			double Totalvalue = beta1 + beta2;
-			double Total = Totalvalue / 2;
+			double Total = Totalvalue / 2; //divides by how many values we have
 					return Total;
 		}
-		public static double average (double kappa1, double kappa2, double kappa3) {
+		public static double average (double kappa1, double kappa2, double kappa3) { // 3 terms accepted
 			double Sum = kappa1 + kappa2 + kappa3;
-			double allSum = Sum / 3;
-			return allSum;
+			double allSum = Sum / 3; 
+			return allSum; 
 		}
-		public static double toRadians (double degree) {
-			double radian = ((degree * 3.14159) / 180);
+		public static double toRadians (double degree) { //From degree to pi
+			double radian = ((degree * 3.14159) / 180); 
 			return radian;
 		}
-		public static double toDegrees (double radian) {
+		public static double toDegrees (double radian) { //opposite function than Radian
 			double degree = ((radian * 180) / 3.14159);
 			return degree;
 		}
 		public static double discriminant (double a, double b, double c) {
-			return b * b - 4 * a * c;
+			return b * b - 4 * a * c; // use in Part4 and only takes doubles
 		}
 		public static int discriminant (int a, int b, int c) {
 			return b * b - 4 * a * c;
 		}
-		public static double toImproperFrac (int a, int b, int c) {
-			return ((a * c) + b) / c;
+		public static String toImproperFrac (int a, int b, int c) {
+			return ((a * c) + b) + "/" + c; //make sure order of operation is true
 		}
 		public static String toMixedNum (int a, int b) {
-			String answer = a / b + "_" + a % b + "/" + 2;
+			String answer = a / b + "_" + a % b + "/" + 2; //used "" in order to add that character not to actually divide
 			return answer;
 		}
-		public static String foil (int a, int b, int c, int d) {
-			int first = a * c;
+		public static String foil (int a, int b, int c, int d) { //do formula (ax + b) (cx + d)
+			int first = a * c; 
 			int outside = a * d;
 			int inside = b *c;
 			int last = a * d;
-			return (first + "n^2 " + outside + inside + "n" + last);
+			return (first + "n^2 " + outside + inside + "n" + last); //made sure to add + for the string part
 		}
 			
 	public static boolean isDivisibleBy (int numerator, int denominator) {
-			if (numerator % denominator == 0) {
+			if (numerator % denominator == 0) { //check for remainder if there is none then its divisible
 				return true;
 			} else {
-				return false;
+				return false; 
 			}
 		}
-	public static double absValue (double a) {
+	public static double absValue (double a) { //simple if it is negative make it positive
 			if (a > 0) {
 				return a;
-			} else {
+			} else { //no need for else if because we have 2 options only
 				return a * -1;
 			}
 		}
 	
-	public static int absValue (int a) {
+	public static int absValue (int a) { //for int for part 4
 		if (a > 0) {
 			return a;
 		} else {
 			return a * -1;
 		}
 	}
-	public static double max (double a, double b) {
+	public static double max (double a, double b) { //checks for max
 			if (a > b) {
 				return a;
 			} else {
 				return b;
 			}
 		}
-	public static double max (double a, double b, double c) {
+	public static double max (double a, double b, double c) { //three values mean more conditions 
 			if (a > b && a > c) {
 				return a;
-			} else if (b > a && b > c){
+			} else if (b > a && b > c){ //used && to save space
 				return b;
 			} else {
 				return c;
 			}
 		}
-			public static int min (int a, int b) {
+			public static int min (int first, int second) { //opposite of max
 				if (a < b) {
 					return a;
 				} else {
 					return b;
 				}
 			}
-			public static double min (double a, double b) {
-				if (a < b) {
-					return a;
+			public static double min (double num1, double num2) { //overload: double
+				if (num1 < num2) {
+					return num1;
 				} else {
-					return b;
+					return num2;
 				}
 			}
 
 	public static double round2 (double x) {
-		double x_hundred = x * 100;
+		double x_hundred = x * 100; //shift x two decimals to right
 		double hundred = x_hundred % 1;
-		double round = (x_hundred - hundred);
+		double round = (x_hundred - hundred); // takes out the same numbers and leave the thousands place we need
 		
-		if (hundred >= 0.5) {
+		if (hundred >= 0.5) { //test for if we need to increment
 			round += 1;
 		}
-		else if (hundred <= -0.5) {
+		else if (hundred <= -0.5) { //test for if we need to decrement
 			round -= 1;
 		}
-		double rounded = round/100;
+		double rounded = round/100; //moves the value 2 places back to the left
 		return rounded;
 
 	}
 	
-	public static double exponent (double a, int b) {
-		double var = 1;
-		for (int i = 0; i < b; i++) {
-			var *= a;	
+	public static double exponent (double base, int power) {
+		double var = 1; 
+		for (int i = 0; i < power; i++) { //no need for <= because it would add another time for loop and mess up the method
+			var *= base;	//basically multiples with itself until loop finsihes
 		}
 		return var;
 	}
 
-	public static int factorial (int a) {
+	public static int factorial (int operand) {
 		int fact = 1;
 	    int i = 1;
-		for(i=1; i <= a; i++) {
+		for(i=1; i <= operand; i++) { //needs equal because it adds the value until
 	    	fact = fact * i;
 	    }
 		return fact;
 	
 	}
-	public static boolean isPrime (int num) {
-		// you might 
+	public static boolean isPrime (int number) {
+		// test if the int is a divisible by 1 and it self
 		boolean prime = true;
-		for (int i = 2; i < num; i++) {
-			if (Calculate.isDivisibleBy(num, i) == true) {
+		for (int i = 2; i < number; i++) {
+			if (Calculate.isDivisibleBy(number, i) == true) { //called for divisibleby to test value
 				
 			}
 		}
-		return prime;
+		return prime; 
     }
 	
 	
 	public static int gcf (int a, int b) {
-		a = Calculate.absValue (a);
+		a = Calculate.absValue (a); //made values positive to make method work
 		b = Calculate.absValue (b);
-		while (a != 0 && b !=0 ) {
-			int c = b;
+		while (a != 0 && b !=0 ) { //test for both zero for efficiency of code
+			int c = b;  //declare new variable 
 			b = a%b;
 			a = c;
 		}
@@ -159,18 +160,18 @@ public class Calculate {
 
 	public static double sqrt (double num) {
 		if (num < 0) {
-			throw new IllegalArgumentException ("Undefined");
+			throw new IllegalArgumentException ("Undefined"); //free check because anything less than zero this program execute
 		}
 		double squareRoot = 1;
-		while (squareRoot * squareRoot > (num + .1) || squareRoot * squareRoot < (num - .1)) {
+		while (squareRoot * squareRoot > (num + .1) || squareRoot * squareRoot < (num - .1)) { 
 			while (squareRoot * squareRoot < num) {
-				squareRoot = squareRoot + .01;
+				squareRoot = squareRoot + .01; // to make the value in between the require boundary
 			}
 			if (squareRoot * squareRoot > num) {
 				squareRoot = squareRoot - .01;
 			}
 		}
-			return (Calculate.round2(squareRoot));
+			return (Calculate.round2(squareRoot)); //rounds it to make sure we don't have 3 demical places
 	}
 	
 	public static String quadform (int num1, int num2, int num3) {
@@ -187,7 +188,7 @@ public class Calculate {
 			else if (ans1 > ans2 || ans1 < ans2) {
 				 answer =  "\"" + Calculate.round2(ans1) + " and " + Calculate.round2(ans2) + "\"";
 						return answer; 
-			}
+		}
 	}
 }
 
